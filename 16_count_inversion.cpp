c011c625ce7709ed0fbb9e09a2e901c85e86b422
vansh7071 <vanshmalhotra7071@gmail.com>
2022-10-07 18:36:16 +0530

    added reverse string in system

--- 16_count_inversion.cpp
+++ 16_count_inversion.cpp
@@ -106,3 +106,15 @@ int main()
     cout << " Number of inversions are " << ans;
     return 0;
 }
+void rvereseArray(int arr[], int start, int end)
+{
+    if (start >= end)
+        return;
+
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+
+    // Recursive Function calling
+    rvereseArray(arr, start + 1, end - 1);
+}
